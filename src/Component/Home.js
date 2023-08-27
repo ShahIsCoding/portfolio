2ea5e6ba12e4ae5a821824a449774f216775eabf
ShahIsCoding <cpabhishekshah0036@hotmail.com>
2023-08-27 16:12:33 +0530

    basic landing page

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,8 +1,7 @@
 import React from "react";
-
 function Home (){
     return (
-        <div className="home d-flex align-items-center justify-content-center flex-wrap p-1">
+        <div className="home d-flex align-items-center justify-content-center flex-wrap flex-column p-1">
             <p >Welcome,</p>
             <p >Myself Abhishek Shah</p>
         </div>
